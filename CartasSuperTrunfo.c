b547f28cae86b9a19b3e4199a20bc5a11a8eeae9
GuhAlcantara <Gustavoamv@gmail.com>
2025-01-15 17:55:50 +0000

    Foi feita todas as adiçoes dos quadrados de perguntas e testado, tudo ok

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,33 +18,38 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    int nome[20];
-    int estado[2];
-    float habitantes;
+    char nome[50] = ("Qual sua cidade?");
+    int area;
+    int habitantes;
     int PIB;
     int numero_de_pontos_turisticos;
 
     printf("Cadastros de carta SuperTrunfo.\n "); 
     
     printf("Digite o nome da cidade:\n ");
-    scanf("%s", &nome);
+    scanf("%50[^\n]", nome); //usado esse formato pois algumas cidades possuem espaço
     
-    printf("Digite as Siglas do estado:\n ");
-    scanf(" %s", &estado);
-
-
     printf("Digite a quantidade de Habitantes:\n ");
     scanf(" %d", &habitantes);
     
+    printf("Digite qual a area da cidade:\n");
+    scanf(" %d", &area);
+
     printf("Digite Qual é o PIB da cidade:\n ");
     scanf(" %d", &PIB);
 
     printf("Digite agora o numero de Pontos turisticos da cidade:\n");
-    scanf(" %f", &numero_de_pontos_turisticos);
+    scanf(" %d", &numero_de_pontos_turisticos);
 
-    printf("Cadastro Efetuado com sucesso:\n");
-    printf("Cidade: %s, nome");
-
+    //a partir daqui sera feita a impressão dos resultados
+    
+    printf("\n-Cadastro feito com Sucesso:\n");
+    printf("\n-Cidade:\n %s \n", nome);
+    printf("-Habitantes:\n %d \n", habitantes);
+    printf("-A area da cidade é:\n %d \n", area);
+    printf("-O PIB da cidade:\n %d \n", PIB);
+    printf("-A Quantidade de Pontos turisticos:\n %d \n", numero_de_pontos_turisticos);
+    printf("Obrigado!");
 
 
     return 0;
